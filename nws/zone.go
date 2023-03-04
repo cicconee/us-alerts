c72c3b8ceea190d9a939d58927898f0a287162dc
Edward Ciccone <edward@Edwards-MacBook-Pro.local>
2023-03-04 15:57:44 -0600

    updated zone.go comments

--- nws/zone.go
+++ nws/zone.go
@@ -2,17 +2,30 @@ package nws
 
 import "time"
 
+// zonesResponse holds the data returned from the https://api.weather.gov/zones?area={area} endpoint.
+// When calling GetZones the response is decoded into zonesResponse.
 type zonesResponse struct {
-	Zones     []zoneFeature `json:"features"`
-	Area      string
+	// Zones corresponds to the json array features. Each feature in the array represents a zone.
+	Zones []zoneFeature `json:"features"`
+
+	// Area is the area code the zones belong to. This is typically a state abbrevation i.e. IL, AK, etc.
+	Area string
+
+	// ExpiresAt holds the date in which the data is no longer valid. This is retrieved from the Expires header.
+	// The NWS api formats this date in a RFC 1123 layout.
 	ExpiresAt time.Time
 }
 
+// zoneFeature holds the data for a specific zone that is returned by the NWS api.
 type zoneFeature struct {
-	URI        string                `json:"id"`
+	// URI is a url to the zone. The format is https://api.weather.gov/zones/{type}/{id}.
+	URI string `json:"id"`
+
+	// Properties holds the details about the zone.
 	Properties zoneFeatureProperties `json:"properties"`
 }
 
+// zoneFeatureProperties holds the details of a zone that is returned by the NWS api.
 type zoneFeatureProperties struct {
 	ID             string    `json:"id"`
 	Type           string    `json:"type"`
@@ -22,6 +35,7 @@ type zoneFeatureProperties struct {
 	State          string    `json:"state"`
 }
 
+// AsAreaZones converts a zonesReponse into a AreaZones.
 func (z *zonesResponse) AsAreaZones() *AreaZones {
 	zones := []Zone{}
 	for _, z := range z.Zones {
@@ -42,12 +56,15 @@ func (z *zonesResponse) AsAreaZones() *AreaZones {
 	}
 }
 
+// AreaZones holds all of the zones for an area. AreaZones is created from zonesResponse and is returned when
+// calling GetZones.
 type AreaZones struct {
 	Zones     []Zone
 	Area      string
 	ExpiresAt time.Time
 }
 
+// Zone holds the data for a zone. Zone is used when returning AreaZones from GetZones.
 type Zone struct {
 	ID             string
 	Type           string
